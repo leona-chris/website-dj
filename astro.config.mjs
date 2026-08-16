f16204bc9435bc6c2adfbb929276752b0cea0656
Leona & Chris <leona.christoph@gmail.com>
2026-08-16 23:35:53 +0200

    restructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/leona-chris',
+  base: '/website-dj/',
   vite: {
     plugins: [tailwindcss()]
   }
